a5eff8354ba46dad35986bc0a9d71a88dad42e67
Dante <dante.novoa@minted.com>
2025-10-07 20:19:06 -0600

    allow cors

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import users
 from mangum import Mangum
 
-
 app = FastAPI(title="Radar API")
+
+# Configuración CORS
+origins = [
+    "*"  # Reemplaza con tu dominio en producción
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 
